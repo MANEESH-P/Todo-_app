5dfc0f8e099edd9c885734f61beb49d2558a5aab
maneesh <mputhenhouse8@gmail.com>
2018-05-26 13:56:15 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
-
+var http =require('http');
 var app = express();
 
 //set up template engine
@@ -16,7 +16,13 @@ todoController(app);
 
 
 
+// app.set('port', process.env.PORT || 3000);
 
  //listen to port
-app.set('port',process.env.PORT || 8000);
-console.log('You are listening to port 8000')
+// app.listen(3000);
+// console.log('You are listening to port 3000')
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
